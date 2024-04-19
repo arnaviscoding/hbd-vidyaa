05b22a56a088023d951ce889ff238514ac074b22
arnaviscoding <138278374+arnaviscoding@users.noreply.github.com>
2024-04-19 10:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var playButton = document.querySelector('.play');
+    var audioPlayer = document.getElementById('audioPlayer');
+    playButton.addEventListener('click', function() {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            
+        } else {
+            audioPlayer.pause();
+        }
+    });
+});
